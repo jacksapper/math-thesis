7cc96070999bd9117c0738b3b22d60ffe0d36462
Jason Hatton <jason.a.hatton@gmail.com>
2016-11-03 10:36:46 -0500

    Made functions only use their local variables instead of variables from main

--- ode.py
+++ ode.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 #---IMPORTS---
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,7 +15,6 @@ D0 = .5*np.asmatrix(np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,
 D1 = (1/INTERVAL_LENGTH)*np.asmatrix(-1*np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,1))
 D = D1-D0
 
-
 #---FUNCTIONS---
 def step_size(u, v, tech='dynamic', size=.00001):
     if tech=='dynamic':
@@ -26,12 +24,12 @@ def step_size(u, v, tech='dynamic', size=.00001):
     elif tech=='static':
         return size
     
-def f(h,n,u):
-    result = (D*ynew.T).T*(D*ynew.T)
+def f(u):
+    result = (D*u.T).T*(D*u.T)
     return .5*result[0,0] 
     
 #u and v need to be column matrices not arrays for the * operator to work correctly
-def df(h, n, u):
+def df(u):
     A=(D.T*D)*u.T
     return A.T
     
@@ -47,14 +45,14 @@ ynew = np.asmatrix(4. * np.ones(POINTS))
 yexact = np.exp(x)
 
 k = 0
-while f(INTERVAL_LENGTH,POINTS-1,ynew) > EPSILON:
-    grad = df(INTERVAL_LENGTH, POINTS, ynew)
-    s = step_size(ynew,grad,'dynamic')
+while f(ynew) > EPSILON:
+    grad = df(ynew)
+    s = step_size(ynew,grad)
     yold = np.copy(ynew)
     ynew = yold - s*grad
-    if k%1000 == 0:
-        print(k, f(INTERVAL_LENGTH,POINTS,ynew))
-    if k%1000 == 0:
+    if k%100 == 0:
+        print(k, f(ynew))
+    if k%100 == 0:
         graph(x.T,ynew.T,yexact.T)
     k=k+1
 
