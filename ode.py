96f39d8626a471a5cf38e2461345a0bb37bbca0c
Jason Hatton <jason.a.hatton@gmail.com>
2016-11-03 09:11:54 -0500

    Removed f_old and df_old which were replaced with functions that use D0 and D1

--- ode.py
+++ ode.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #---CONSTANTS---
 LBOUND = 0.
-UBOUND = 1.
+UBOUND = 2.
 POINTS = 50
 EPSILON = 10**-3
 
@@ -26,29 +26,12 @@ def step_size(u, v, tech='dynamic', size=.00001):
     
 def f(h,n,u):
     result = (D*ynew.T).T*(D*ynew.T)
-    return .5*result[0,0]
-    
-def f_old(h, n, u):
-    result = 0
-    for i in range(1,n):
-        result += ( ((u[0,i] - u[0,i-1])/(h)) - ((u[0,i] + u[0,i-1]) / 2.) )**2
-    result *= (1/2.)
-    return result
+    return .5*result[0,0] 
     
 #u and v need to be column matrices not arrays for the * operator to work correctly
 def df(h, n, u):
     A=(D.T*D)*u.T
     return A.T
-    
-def df_old(h, n, u):
-    result = np.ones(n+1)
-    for i in range(1,n-1):
-        #wrapping indices problem with initial conditions
-        result[i] = (((u[i] - u[i-1])/(h)) - ((u[i] + u[i-1]) / 2.))*((1/h)-(1/2.)) + (((u[i+1] - u[i])/(h)) - ((u[i+1] + u[i]) / 2.))*((-1/h)-(1/2.))
-    #result[0] = (((u[i+1] - u[i])/(h)) - ((u[i+1] + u[i]) / 2.))*((-1/h)-(1/2.))
-    result[0] = 0
-    result[n] = (((u[i] - u[i-1])/(h)) - ((u[i] + u[i-1]) / 2.))*((1/h)-(1/2.))
-    return result
     
 def graph(x,y1,y2):
     plt.plot(x,y1)
@@ -67,10 +50,10 @@ while f(INTERVAL_LENGTH,POINTS-1,ynew) > EPSILON:
     s = step_size(ynew,grad,'static')
     yold = np.copy(ynew)
     ynew = yold - s*grad
-    if k%100 == 0:
-        print(k, f(INTERVAL_LENGTH,POINTS,ynew))
     if k%1000 == 0:
-        graph(x,ynew,yexact)
+        print(k, f(INTERVAL_LENGTH,POINTS,ynew))
+#    if k%1000 == 0:
+#        graph(x,ynew,yexact)
     k=k+1
 
 graph(x,ynew,yexact)
