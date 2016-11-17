7f394dbeffd7cb8c016cb6947911e5f2a71f0541
Jason Hatton <jason.a.hatton@gmail.com>
2016-11-17 09:15:58 -0600

    changed initial defintions of x, yold, ynew to transposes to clean up matrix math.

--- ode.py
+++ ode.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 #---CONSTANTS---
 LBOUND = 0.
-UBOUND = 2.
+UBOUND = 1.
 POINTS = 50
 EPSILON = 10**-3
+INITIAL = 2
 
 #---DERIVED CONSTANTS---DON'T CHANGE THESE, CHANGE THE REAL CONSTANTS
 INTERVAL_LENGTH = (UBOUND-LBOUND)/(POINTS-1)
@@ -15,27 +16,28 @@ D0 = .5*np.asmatrix(np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,
 D1 = (1/INTERVAL_LENGTH)*np.asmatrix(-1*np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,1))
 D = D1-D0
 A = D1.T * D1 + D0.T * D0
+
+
 #---FUNCTIONS---
 def step_size(u, v, tech='dynamic', size=.00001):
     if tech=='dynamic':
-        upper = u*v.T
-        lower = v*v.T
+        upper = u.T*v
+        lower = v.T*v
         return upper[0,0]/lower[0,0]
     elif tech=='static':
         return size
     
 def f(u):
-    result = (D*u.T).T*(D*u.T)
+    result = (D*u).T*(D*u)
     return .5*result[0,0] 
     
 #u and v need to be column matrices not arrays for the * operator to work correctly
 def df(u):
-    A=(D.T*D)*u.T
-    return A.T
+    A=(D.T*D)*u
+    return A
     
 def sobolev(u):
-    return np.linalg.solve(A,u.T)
-    
+    return np.linalg.solve(A,u)
     
 def graph(x,y1,y2):
     plt.plot(x,y1)
@@ -43,23 +45,23 @@ def graph(x,y1,y2):
     plt.show()
     
 #---MAIN---
-x = np.asmatrix(np.linspace(LBOUND,UBOUND,POINTS))
-yold = np.asmatrix(np.zeros(POINTS))
-ynew = np.asmatrix(12. * np.ones(POINTS))
-#ynew = np.cos(x)
-yexact = np.exp(x)
+x = np.asmatrix(np.linspace(LBOUND,UBOUND,POINTS)).T
+yold = np.asmatrix(np.zeros(POINTS)).T
+ynew = np.asmatrix(12. * np.ones(POINTS)).T
+yexact = 2*np.exp(x)
 
+ynew[0,0] = INITIAL
 k = 0
 while f(ynew) > EPSILON:
-    grad = sobolev(df(ynew)).T
-    s = step_size((D*ynew.T).T,(D*grad.T).T)
+    grad = sobolev(df(ynew))
+    s = step_size((D*ynew),(D*grad),'dynamic')
     yold = np.copy(ynew)
     ynew = yold - s*grad
     if k%100 == 0:
         print(k, f(ynew))
-    if k%100 == 0:
-        graph(x.T,ynew.T,yexact.T)
+    if k%1000 == 0:
+        graph(x,ynew,yexact)
     k=k+1
 
-graph(x.T,ynew.T,yexact.T)
+graph(x,ynew,yexact)
 print(k)
