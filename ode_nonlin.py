4293ce490421ec813716158018d6be8b96dca02c
Jason Hatton <jason@hatton.io>
2017-02-13 10:23:23 -0600

    Non-linear working code

--- ode_nonlin.py
+++ ode_nonlin.py
@@ -6,36 +6,33 @@ import numpy.linalg as lin
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
 #---CONSTANTS---
-LBOUND = -1.
-UBOUND = 1.
+LBOUND = 1.
+UBOUND = 3.
 POINTS = 2**6
 EPSILON = 10**-16
 INITIAL = None
-
+STEP_SIZE = .1
+STEP_SIZE_TECH = 'static'
 #Matrix is O(POINTS**2)
 
 #---DERIVED CONSTANTS---DON'T CHANGE THESE, CHANGE THE REAL CONSTANTS
 INTERVAL_LENGTH = (UBOUND-LBOUND)/(POINTS-1)
-<<<<<<< HEAD
-D0 = (.5*(np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,1)))
-D1 = ((1/INTERVAL_LENGTH)*(-1*np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,1)))
-=======
 D0 = .5*(np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,1))
 D1 = (1/INTERVAL_LENGTH)*(-1*np.eye(POINTS-1,POINTS) + np.roll(np.eye(POINTS-1,POINTS),1,1))
->>>>>>> d7586146040a05c90e1c8ca1f578a2e5793b2b90
 #D = D1 + D0
 k = 0
 A = D1.T @ D1 + D0.T @ D0
 
 #---FUNCTIONS---        
-def step_size(u, v, tech='dynamic', size=1):
+def step_size(u, v,tech=STEP_SIZE_TECH,size=STEP_SIZE):
     if tech=='dynamic':
         upper = u.dot(v)
         lower = v.dot(v)
         return upper/lower
     elif tech=='static':
+        print(size)
         return size
-    
+
 def f(u): #change discretized function here
     return D1 @ u + D0 @ (u**2)
     #return D1 @ u + (D0 @ u)**2
@@ -48,6 +45,8 @@ def phi(f):
     return .5*f.dot(f)
     
 def dphi(f,df):
+    if INITIAL is not None:
+        df[index] = 0
     return df.T @ f
     
     
@@ -61,7 +60,6 @@ def graph(x,y1):
     
 #---MAIN---
 x = np.linspace(LBOUND,UBOUND,POINTS)
-#y0 = np.zeros(POINTS)
 y = np.random.rand(POINTS)
 #yexact = np.exp(x)
 
@@ -69,17 +67,12 @@ if INITIAL is not None:
     index = np.argmin(abs(x-INITIAL[0]))
     y[index] = INITIAL[1]
 
-while phi(f(y)) > EPSILON and np.isfinite(phi(f(y))):
-<<<<<<< HEAD
+s = STEP_SIZE
+while phi(f(y)) > EPSILON or not np.isfinite(phi(f(y))):
+    if(not np.isfinite(phi(f(y)))):
+        y = np.random.rand(POINTS)
     grad =  sobolev(dphi( f(y), df(y) ))
-    s = step_size(y,grad,'static')
-=======
-    grad =  dphi( f(y), df(y) )
-    if INITIAL is not None:
-        grad[index] = 0
-    s = step_size(f(y),f(grad),'dynamic')
->>>>>>> d7586146040a05c90e1c8ca1f578a2e5793b2b90
-    y0 = np.copy(y)
+    s = step_size(y,grad)
     y -= s*grad
     if k%2 == 0:
         print(k, phi(f(y)))
